094da6f267a2a751b1e8f4aca2a4c137ad58d95d
dolfo.venancio@gmail.com <dolfo.venancio@gmail.com>
2020-10-21 21:58:07 +0100

    Project template and initial config

---
+++ src/main/kotlin/com/personio/hierarchy/employee_hierarchy/EmployeeHierarchyApplication.kt
@@ -0,0 +1,11 @@
+package com.personio.hierarchy.employee_hierarchy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmployeeHierarchyApplication
+
+fun main(args: Array<String>) {
+	runApplication<EmployeeHierarchyApplication>(*args)
+}
---
+++ src/test/kotlin/com/personio/hierarchy/employee_hierarchy/EmployeeHierarchyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.personio.hierarchy.employee_hierarchy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmployeeHierarchyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
