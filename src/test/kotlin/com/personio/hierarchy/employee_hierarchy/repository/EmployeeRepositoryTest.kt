e7c521d824fe88464e983d53935a7673e40a0292
dolfo.venancio@gmail.com <dolfo.venancio@gmail.com>
2020-10-24 20:17:23 +0100

    Added simple test to getEmployeeSupervisors

--- src/test/kotlin/com/personio/hierarchy/employee_hierarchy/repository/EmployeeRepositoryTest.kt
+++ src/test/kotlin/com/personio/hierarchy/employee_hierarchy/repository/EmployeeRepositoryTest.kt
@@ -1,6 +1,7 @@
 package com.personio.hierarchy.employee_hierarchy.repository
 
 import com.personio.hierarchy.employee_hierarchy.model.entity.Employee
+import com.personio.hierarchy.employee_hierarchy.model.response.EmployeeSupervisorsResponse
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -21,6 +22,18 @@ class EmployeeRepositoryTest(@Autowired val employeeRepository: EmployeeReposito
         Assertions.assertEquals("TEST_EMPLOYEE", employee?.name);
         Assertions.assertNotNull(employee?.supervisorId);
         Assertions.assertEquals(9998, employee?.supervisorId);
+    }
+
+    @SqlGroup(
+            Sql(scripts = arrayOf("classpath:data/sql/insert_employees.sql"), executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+            Sql(scripts = arrayOf("classpath:data/sql/delete_employees.sql"), executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD))
+    @Test
+    fun getEmployeeSupervisorsTest() {
+        val employee: EmployeeSupervisorsResponse? = employeeRepository.findSupervisorsByEmployeeName("TEST_EMPLOYEE");
+        Assertions.assertNotNull(employee);
+        Assertions.assertEquals("TEST_EMPLOYEE", employee?.employee);
+        Assertions.assertEquals("SUPERVISOR2", employee?.supervisor);
+        Assertions.assertEquals("SUPERVISOR1", employee?.supervisorOfSupervisor);
     }
 
     @Sql(scripts = arrayOf("classpath:data/sql/insert_employees.sql"), executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
